fc10d4541f860160466f147b883c9126e7a305ea
s24967-pj <s24967@pjwstk.edu.pl>
2024-12-19 23:09:26 +0100

    Adding file requirements.txt and documentation

--- Banknote_authentication_AI.py
+++ Banknote_authentication_AI.py
@@ -6,7 +6,20 @@ from tensorflow.keras.layers import Dense
 from sklearn.metrics import classification_report
 from tensorflow.keras.optimizers import Adam
 
-def read_CSV(filename):
+"""
+Autorzy:
+Daria Szabłowska s24967
+Damian Grzesiak s25866
+
+Aby uruchomić program należy:
+Zainstalować wymagane biblioteki, korzystając z pliku requirements.txt.
+W tym celu używając poniższej komendy w terminalu: pip install -r requirements.txt
+
+Program implementuje proces uczenia maszynowego aby sklasyfikować dane dotyczące autentykacji banknotów. 
+"""
+
+def read_file(filename):
+    """Funkcja do odczytywania pliku jako 'read' i przekształcajaca go w obiekt DataFrame"""
     with open(filename, 'r') as file:
         lines = file.readlines()
     data = [line.strip().split(",") for line in lines]
@@ -14,30 +27,40 @@ def read_CSV(filename):
 
 file_path = "data_banknote_authentication.txt"
 try:
-    data = read_CSV(file_path)
+    data = read_file(file_path)
 except FileNotFoundError:
     print(f"File {file_path} not found. Please provide a valid path.")
     exit()
-
+# Wyświetlenie wczytanych danych
 print(data)
-X = data.iloc[:, :-1].values
-y = data.iloc[:, -1].values
+
+# Podział na cechy - X i etykiety -y
+X = data.iloc[:, :-1].values #bez ostatniej
+y = data.iloc[:, -1].values #ostatnia jako wynik
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Skalowanie danych za pomocą StandardScaler
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)  # poprawka na transformowanie, a nie dopasowywanie
 
+# Tworzenie modelu sieci neuronowej - koejno warstwa: wejściowa, ukryta, wyjściowa
 model = Sequential([
     Dense(16, activation='relu', input_shape=(X_train.shape[1],)),
     Dense(8, activation='relu'),
     Dense(1,activation='sigmoid')
 ])
+
+# Funkcja strat i metryka dokładności
 model.compile(optimizer=Adam(learning_rate=0.01),loss='binary_crossentropy',metrics=['accuracy'])
+
+# Trenowanie modelu na zbiorze treningowym i ocena na zbiorze testowym
 history=model.fit(X_train, y_train, epochs=50,batch_size=4, validation_split=0.2,verbose=2)
 loss, accuracy = model.evaluate(X_test, y_test,verbose=0)
 print(f"Loss: {loss}, Accuracy: {accuracy}")
 
+# Przewidywanie etykiet 
 predictions = (model.predict(X_test)>0.5).astype(int)
 
 print("\nClassification Report:\n")
--- cifar10_neural_network.py
+++ cifar10_neural_network.py
@@ -4,20 +4,37 @@ from keras import datasets, layers, models
 from keras import preprocessing
 import numpy as np
 
+"""
+Autorzy:
+Daria Szabłowska s24967
+Damian Grzesiak s25866
+
+Aby uruchomić program należy:
+Zainstalować wymagane biblioteki, korzystając z pliku requirements.txt.
+W tym celu używając poniższej komendy w terminalu: pip install -r requirements.txt
+
+W projekcie używany jest dataset CIFAR10, na podstawie którego uczymy odróżniać obiekty na zdjęciach.
+"""
+
+# Ładowanie datasetu CIFAR10
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
+# Normalizacja wartości do 0 i 1
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
+# Wizualizacja 25 przykładowych obrazów z etykietami klas
 plt.figure(figsize=(10,10))
 for i in range(25):
     plt.subplot(5,5,i+1)
+# Wyłączenie osi i siatki
     plt.xticks([]), plt.yticks([]), plt.grid(False)
     plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i][0]])
 plt.show()
 
+# Tworzenie modelu sieci neuronowej 
 model = models.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
     layers.MaxPooling2D((2, 2)),
@@ -29,20 +46,33 @@ model = models.Sequential([
     layers.Dense(10)
 ])
 
+# Podsumowanie modelu
 model.summary()
 
+#Funkcja strat i metryka dokładności
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
+# Trenowanie modelu na danych treningowych i walidacja na danych testowych
 history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
 
 def predict_image(img_path):
+    
+    """
+    Funkcja przewidująca klasę dla podanego obrazu i wyświetlająca go z nazwą przewidywanej klasy.
+    """
+    
+    # Wczytanie i przetworzenie obrazka
     img = preprocessing.image.load_img(img_path, target_size=(32, 32))
     img_array = preprocessing.image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
+
+    # Przewidywanie co znajduje sie na obrazku
     predictions = model.predict(img_array)
     predicted_class = class_names[np.argmax(predictions)]
+
+    # Wyświetlanie
     plt.imshow(img)
     plt.title(f'Predicted: {predicted_class}')
     plt.axis('off')
